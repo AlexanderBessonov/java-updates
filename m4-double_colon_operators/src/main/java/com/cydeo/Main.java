0b8a32244f2071b28d85786262c3866aad067c3b
Alexander Bessonov <bessonovUSA@gmail.com>
2022-08-18 11:05:41 -0500

    Double colon operator

---
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,33 @@
+package com.cydeo;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+
+      //Predicate<Integer> pred = (p)-> p % 2 == 0 ? true : false;
+        Calculate sum = (x,y)-> System.out.println(x+y);
+        Calculate s1 = (x,y)-> Calculator.findSum(x,y); //from class
+
+        //*****Reference to a Static Method*****
+        Calculate s2 = Calculator::findSum;
+        s2.calculate(10,20);
+
+        //*****Reference to a instance method
+
+        Calculator obj = new Calculator();
+        Calculate s3 = obj :: findMultiply;
+
+        Calculate s4 = new Calculator()::findMultiply;
+
+        BiFunction<String,Integer,String> func = (str,i)->str.substring(i);
+        BiFunction<String,Integer,String> func2 = String::substring;
+
+        Function<Integer,Double> b = new MyClass()::method;
+        BiFunction<MyClass,Integer,Double> b1 = MyClass::method;
+
+    }
+
+}
